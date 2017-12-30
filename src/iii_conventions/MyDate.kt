cac73fe8025d9452a82c5f7555b0b37b4b3032ee
sevi <sevi0706@gmail.com>
2017-12-30 17:45:30 +0100

    25,26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,21 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return if(this.year==other.year)
+                    if (this.month==other.month)
+                        if (this.dayOfMonth==other.dayOfMonth)
+                            0
+                        else
+                            this.dayOfMonth-other.dayOfMonth
+                    else
+                        this.month-other.month
+                else
+                    this.year-other.year
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
@@ -9,5 +24,7 @@ enum class TimeInterval {
     WEEK,
     YEAR
 }
+operator fun DateRange.contains(other: MyDate): Boolean =
+        this.start.compareTo(other)<=0 && this.endInclusive.compareTo(other)>=0
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
