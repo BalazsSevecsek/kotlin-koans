e1f160ea7596f52d6e5904aac4bec135e8bcc977
sevi <sevi0706@gmail.com>
2018-01-01 19:05:54 +0100

    32,33

--- src/iv_properties/n33LazyProperty.kt
+++ src/iv_properties/n33LazyProperty.kt
@@ -3,7 +3,16 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    private var isLazyInitialized = false
+
+    val lazy: Int
+        get() {
+            if(!isLazyInitialized) {
+                isLazyInitialized = true
+                return initializer()
+            }
+            return field
+        }
 }
 
 fun todoTask33(): Nothing = TODO(
